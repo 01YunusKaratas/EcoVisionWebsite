44b44d3eda537a6fd1f8c818b2d7597a8c5b0777
01YunusKaratas <yunuskaratas589@gmail.com>
2025-02-09 05:18:49 +0300

    ..

--- Js/script.js
+++ Js/script.js
@@ -2,14 +2,18 @@
 window.addEventListener("scroll",function () {
     let navbarDom = document.querySelector(".navbar");//ilk gelen navbar classını alır
     let logoDom = this.document.getElementById("logo");//burada logoyuda eklicez
+    let includesDom = this.document.getElementById("includes");//ustten margin
     if (window.innerWidth > 480) {
         if (this.window.scrollY > 100) {
             navbarDom.classList.add("scrolled");
             logoDom.style.display = "block";
+            
         } else {
             navbarDom.classList.remove("scrolled");
             logoDom.style.display = "none";
         }
+    }else{
+        includesDom.style.marginTop="150px";
     }
     
 
--- README.md
+++
@@ -1,1 +0,0 @@
-# renderdeneme2
